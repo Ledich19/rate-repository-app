b7b45f434165950b64e5ba43b451a7966fbb6f76
Aleksandr <ledich19@gmail.com>
2023-08-06 09:33:42 +0300

    init: start rate repository project

--- App.js
+++ App.js
@@ -1,20 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StyleSheet, Text, View, Pressable, Alert
+} from 'react-native';
 
-export default function App() {
+const BACKGROUND_COLOR = '#ffffff';
+
+const App = () => {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      {/* eslint-disable-next-line react/style-prop-object */}
       <StatusBar style="auto" />
+
+      <Pressable onPress={() => Alert.alert('You pressed the text!')}>
+        <Text>You can press me</Text>
+      </Pressable>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: BACKGROUND_COLOR,
     alignItems: 'center',
     justifyContent: 'center',
   },
 });
+
+export default App;
